d6393d08a39f4b8748387d24bf2a15877bb505f5
Kevin Gliewe <kevingliewe@gmail.com>
2017-10-17 18:37:54 +0200

    Fix namespaces

--- GCore/Extensions/IDictionary/IDictionaryExtensions.cs
+++ GCore/Extensions/IDictionary/IDictionaryExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ErhardtAbt.Extensions.IDictionaryEx {
+namespace GCore.Extensions.IDictionaryEx {
     public static class IDictionaryExtensions {
         /// <summary>
         /// Gibt den Wehrt für den Key zurück oder den Standartwehrt,
--- GCore/Extensions/Size/SizeExtensions.cs
+++ GCore/Extensions/Size/SizeExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 
-namespace ErhardtAbt.Extensions.SizeEx {
+namespace GCore.Extensions.SizeEx {
     public static class SizeExtensions {
         /// <summary>
         /// Addiert zu der Goße den übergebenen Wehrt hinzu.
