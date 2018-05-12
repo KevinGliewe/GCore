4f522d2379b281401584b4738ce4e7269c50f36b
Kevin Gliewe <kevingliewe@googlemail.com>
2018-05-12 22:39:37 +0200

    Add async FileInfo extensions and add logger for Debug.WriteLine [1.0.5]

--- GCore/Extensions/FileInfo/FileInfoExtensions.cs
+++ GCore/Extensions/FileInfo/FileInfoExtensions.cs
@@ -5,9 +5,12 @@ using System.Text;
 using System.IO;
 using GCore.Extensions.SerializingEx;
 using GCore.Extensions.IEnumerableEx;
+using System.Threading.Tasks;
 
 namespace GCore.Extensions.FileInfoEx {
     public static class FileInfoExtensions {
+
+        #region Sync
         /// <summary>
         /// Schreibt den Strine in die Datei.
         /// </summary>
@@ -95,6 +98,97 @@ namespace GCore.Extensions.FileInfoEx {
         public static T GetObjectXML<T>(this FileInfo @thisX) {
             return @thisX.GetString().DeserializeXML<T>();
         }
+        #endregion
+
+        #region Async
+        /// <summary>
+        /// Schreibt den Strine in die Datei.
+        /// </summary>
+        /// <param name="thisX"></param>
+        /// <param name="content"></param>
+        public static async Task SetStringAsync(this FileInfo @thisX, string content) {
+            FileStream fileStream = @thisX.OpenWrite();
+            using(StreamWriter streamWriter = new StreamWriter(fileStream))
+                await streamWriter.WriteAsync(content);
+        }
+
+        /// <summary>
+        /// Liest die Datei ein und gibt sie als String zurück.
+        /// </summary>
+        public static async Task<string> GetStringAsync(this FileInfo @thisX) {
+            FileStream fileStream = @thisX.OpenRead();
+            using(StreamReader streamReader = new StreamReader(fileStream))
+                return await streamReader.ReadToEndAsync();
+        }
+
+        /// <summary>
+        /// Serialisiert das Objekt und schreibt es in die Datei.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="thisX"></param>
+        /// <param name="content"></param>
+        /// <param name="serializer"></param>
+        public static void SetObjectAsync<T>(this FileInfo @thisX, T content, SerializingExtensions.Serializer serializer) {
+            @thisX.SetStringAsync(content.Serialize(serializer));
+        }
+
+        /// <summary>
+        /// Liest die Datei ein und Deserialisiert den Inhalt zu einem Objekt.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="thisX"></param>
+        /// <param name="serializer"></param>
+        /// <returns></returns>
+        public static async Task<T> GetObjectAsync<T>(this FileInfo @thisX, SerializingExtensions.Serializer serializer) {
+            return (await @thisX.GetStringAsync()).Deserialize<T>(serializer);
+        }
+
+        /// <summary>
+        /// Serialisiert das Objekt mithilfe das "BinaryFormatterr"
+        /// und schreibt es in die Datei.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="thisX"></param>
+        /// <param name="content"></param>
+        public static Task SetObjectBinaryAsync<T>(this FileInfo @thisX, T content) {
+            return @thisX.SetStringAsync(content.SerializeBinary());
+        }
+
+        /// <summary>
+        /// Liest die Datei ein und Deserialisiert
+        /// den Inhalt mithilfe des "BinaryFormatterr"
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="thisX"></param>
+        /// <returns></returns>
+        public static async Task<T> GetObjectBinaryAsync<T>(this FileInfo @thisX) {
+            return (await @thisX.GetStringAsync()).DeserializeBinary<T>();
+        }
+
+        /// <summary>
+        /// Serialisiert das Objekt mithilfe das "SoapFormatterr"
+        /// und schreibt es in die Datei.
+        /// </summary>
+        public static Task SetObjectSoapAsync<T>(this FileInfo @thisX, T content) {
+            return @thisX.SetStringAsync(content.SerializeSoap());
+        }
+
+        /// <summary>
+        /// Liest die Datei ein und Deserialisiert
+        /// den Inhalt mithilfe des "SoapFormatterr"
+        /// </summary>
+        public static async Task<T> GetObjectSoapAsync<T>(this FileInfo @thisX) {
+            return (await @thisX.GetStringAsync()).DeserializeSoap<T>();
+        }
+
+        public static Task SetObjectXMLAsync<T>(this FileInfo @thisX, T content) {
+            return @thisX.SetStringAsync(content.SerializeXML());
+        }
+
+        public static async Task<T> GetObjectXMLAsync<T>(this FileInfo @thisX) {
+            return (await @thisX.GetStringAsync()).DeserializeXML<T>();
+        }
+        #endregion
 
         public static string GetFilenameWithoutExtension(this FileInfo @thisX) {
             string[] parts = @thisX.Name.Split('.');
---
+++ GCore/Logging/Logger/DebugLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace GCore.Logging.Logger
+{
+    public class DebugLogger : ILoggingHandler {
+        public void Debug(DateTime timestamp, string Message, StackTrace Stacktrace, params object[] list) {
+        }
+
+        public void Error(DateTime timestamp, string Message, StackTrace Stacktrace, params object[] list) {
+        }
+
+        public void Exaption(DateTime timestamp, string Message, Exception exception, StackTrace Stacktrace, params object[] list) {
+        }
+
+        public void Fatal(DateTime timestamp, string Message, StackTrace Stacktrace, params object[] list) {
+        }
+
+        public void Info(DateTime timestamp, string Message, StackTrace Stacktrace, params object[] list) {
+        }
+
+        public void Success(DateTime timestamp, string Message, StackTrace Stacktrace, params object[] list) {
+        }
+
+        public void Warn(DateTime timestamp, string Message, StackTrace Stacktrace, params object[] list) {
+        }
+
+        public void General(LogEntry logEntry) {
+            string Text = logEntry.TimeStamp.ToString("yyyy-MM-dd H:mm:ss.fff") + "\t";
+            Text += logEntry.LogType.ToString() + "\t";
+            Text += logEntry.Message + "\t";
+            Text += logEntry.StackTrace.ToString().Split('\n')[1].Replace("\r", "").Trim() + "\t";
+            Text += logEntry.Thread.Name + "\t";
+            if(logEntry.Exception != null) Text += logEntry.Exception.ToString() + "\t";
+            foreach(Object o in logEntry.Params)
+                Text += o.ToString() + ";";
+
+            System.Diagnostics.Debug.WriteLine(Text);
+        }
+    }
+}
