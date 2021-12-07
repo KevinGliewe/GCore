c53f7ea68248927b03a67b6c322ccdfc9a76c442
Kevin Gliewe <kevingliewe@outlook.de>
2021-12-07 18:27:22 +0100

    Add RegexMatchFilter

---
+++ src/GCore/Data/Filter/RegexMatchFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GCore.Data.Filter;
+
+public class RegexMatchFilter : IFilter<string>
+{
+    public Regex Pattern { get; private set; }
+
+    public RegexMatchFilter(Regex pattern)
+    {
+        Pattern = pattern;
+    }
+
+    public RegexMatchFilter(string pattern) : this(new Regex(pattern)) { }
+
+    public bool Passes(string elem) => Pattern.IsMatch(elem);
+}
