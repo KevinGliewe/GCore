9b84e6ccb9500a2a841d817e49630122e501678e
Kevin Gliewe <kevingliewe@outlook.de>
2021-12-07 18:28:21 +0100

    Fix toolStripContainer Anchor #patch

--- src/GCore.AppSystem.WinForms/MainForm.Designer.cs
+++ src/GCore.AppSystem.WinForms/MainForm.Designer.cs
@@ -118,6 +118,9 @@ namespace GCore.AppSystem.WinForms
             // 
             // toolStripContainer
             // 
+            this.toolStripContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             // 
             // toolStripContainer.ContentPanel
             // 
